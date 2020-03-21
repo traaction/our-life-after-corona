d8dd9c0fb44dae3c789aa15822345b73e7480c27
Jens Böttcher <eljenso.boettcher@gmail.com>
2020-03-22 00:40:59 +0100

    Get activities

--- src/components/ActivityScreen/index.tsx
+++ src/components/ActivityScreen/index.tsx
@@ -5,49 +5,27 @@ import { UUID } from "../../types";
 
 interface IProps {
   onNext: () => void;
-  setUserName: React.Dispatch<React.SetStateAction<string>>;
   setActivity: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export function ActivityScreen({
-  onNext,
-  setUserName,
-  setActivity
-}: IProps): JSX.Element {
-  const [userNameInput, setUserNameInput] = useState<string>("");
+export function ActivityScreen({ onNext, setActivity }: IProps): JSX.Element {
   const [activityUuidInput, setActivityUuid] = useState<UUID>("");
-  const [showActivityInput, setShowActivityInput] = useState<boolean>(false);
 
   return (
     <>
-      <>
-        <span>Hi, my name is</span>
-        <TextField
-          onChange={event => {
-            setUserNameInput(event.target.value);
-          }}
-          onBlur={event => setShowActivityInput(true)}
-        />
-      </>
+      <p>After the corona pandemic, I want to</p>
+      <SelectActivity {...{ setActivityUuid }} />
 
-      {showActivityInput && (
-        <>
-          <p>and after the corona pandemic, I want to</p>
-          <SelectActivity {...{ setActivityUuid }} />
-
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              setUserName(userNameInput);
-              setActivity(activityUuidInput);
-              onNext();
-            }}
-          >
-            Create
-          </Button>
-        </>
-      )}
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          setActivity(activityUuidInput);
+          onNext();
+        }}
+      >
+        Create
+      </Button>
     </>
   );
 }
