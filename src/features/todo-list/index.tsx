78c098932e742d8e2b5b8b41b30b9011b2454bab
Jens Böttcher <eljenso.boettcher@gmail.com>
2020-03-22 19:33:07 +0100

    Render user's todos as list

--- src/features/todo-list/index.tsx
+++ src/features/todo-list/index.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { UUID } from "types";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import React, { useEffect, useState } from "react";
+import { ISentence, UUID } from "types";
+import { getSentencesForUser } from "utils/api";
 
 interface IProps {
   userUuid: UUID;
 }
 
 export function TodoList({ userUuid }: IProps): JSX.Element {
-  return <div>asdasd</div>;
+  const [userSentences, setUserSentences] = useState<ISentence[]>([]);
+
+  useEffect(() => {
+    if (userUuid) {
+      getSentencesForUser(userUuid).then(sentences => {
+        setUserSentences(sentences);
+      });
+    }
+  }, [userUuid]);
+
+  return (
+    <List>
+      {userSentences.map(
+        (sentence): JSX.Element => {
+          return (
+            <ListItem key={sentence.uuid}>
+              <ListItemText
+                primary={sentence.activity.name}
+                secondary={sentence.place.name}
+              />
+            </ListItem>
+          );
+        }
+      )}
+    </List>
+  );
 }
