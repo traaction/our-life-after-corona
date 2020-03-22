ee9df68531d936e0e034945db9755ac16cff881d
Jens Böttcher <eljenso.boettcher@gmail.com>
2020-03-22 09:10:02 +0100

    Remove setUserName prop usage

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import { PlaceScreen } from "./components/PlaceScreen";
 export function App(): JSX.Element {
   const [showActivityScreen, setShowActivityScreen] = useState<boolean>(true);
 
-  const [userName, setUserName] = useState<string>("");
   const [activityUuid, setActivityUuid] = useState<string>("");
   const [placeUuid, setPlaceUuid] = useState<string>("");
 
   useEffect(() => {
-    console.log({ showActivityScreen, userName, activityUuid, placeUuid });
-  }, [showActivityScreen, userName, activityUuid, placeUuid]);
+    console.log({ showActivityScreen, activityUuid, placeUuid });
+  }, [showActivityScreen, activityUuid, placeUuid]);
 
   return (
     <div className="App">
       {showActivityScreen && (
         <ActivityScreen
-          setUserName={setUserName}
           setActivity={setActivityUuid}
           onNext={() => {
             setShowActivityScreen(false);
