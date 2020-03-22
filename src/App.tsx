6ab6cd9a488bbf14da8a7fd17c728058654df6c0
Jens Böttcher <eljenso.boettcher@gmail.com>
2020-03-22 19:33:14 +0100

    Use tabbed navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import { TodoList } from "features/todo-list";
 import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Route, Switch, useHistory } from "react-router-dom";
 import uuidV4 from "uuid/v4";
 import { SentenceCreation } from "./features/sentence-creation";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-
-import { useCookies } from "react-cookie";
-import { TodoList } from "features/todo-list";
 
 const COOKIE_NAME = "our-life-after-corona_user-uuid";
+
+const HOME_PATH = "/";
+const LIST_PATH = "/list";
 
 export function App(): JSX.Element {
   const [cookies, setCookie] = useCookies([COOKIE_NAME]);
   const [userUuid, setUserUuid] = useState<string>("");
+  const [currentTabIndex, setCurrentTabIndex] = useState<number>(0);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    let nextPath: string;
+    switch (currentTabIndex) {
+      case 1: {
+        nextPath = LIST_PATH;
+        break;
+      }
+
+      case 0:
+      default: {
+        nextPath = HOME_PATH;
+        break;
+      }
+    }
+    history.push(nextPath);
+  }, [currentTabIndex, history]);
 
   // Setup cookie with userID
   useEffect(() => {
@@ -26,27 +51,30 @@ export function App(): JSX.Element {
 
   return (
     <Container maxWidth="sm">
-      <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/list">Your ToDos</Link>
-            </li>
-          </ul>
-        </nav>
+      <Paper square>
+        <Tabs
+          value={currentTabIndex}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          onChange={(event: React.ChangeEvent<{}>, nextIndex: number) => {
+            setCurrentTabIndex(nextIndex);
+          }}
+          aria-label="disabled tabs example"
+        >
+          <Tab label="Home" />
+          <Tab label="Your ToDos" />
+        </Tabs>
+      </Paper>
 
-        <Switch>
-          <Route path="/list">
-            <TodoList {...{ userUuid }} />
-          </Route>
-          <Route exact path="/">
-            <SentenceCreation {...{ userUuid }} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route path="/list">
+          <TodoList {...{ userUuid }} />
+        </Route>
+        <Route exact path="/">
+          <SentenceCreation {...{ userUuid }} />
+        </Route>
+      </Switch>
     </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { CookiesProvider } from "react-cookie";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <BrowserRouter>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
